9e8b88e8347ac570e87ca5734c631dd986f47ccb
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-02-27 20:05:09 +0200

    More solutions

--- 02-JS-ARRAYS-&-STRINGS/EXERCISE/04-sorting-numbers.js
+++ 02-JS-ARRAYS-&-STRINGS/EXERCISE/04-sorting-numbers.js
@@ -6,11 +6,16 @@ function solve(arr) {
         let min = sortedArr.shift()
         let max = sortedArr.pop()
 
-        newArr.push(min)
-        newArr.push(max)
+        if (min) {
+            newArr.push(min)
+        }
+
+        if (max) {
+            newArr.push(max)
+        }
     }
 
     return newArr
 }
 
-console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]))
+console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56, 100]))
