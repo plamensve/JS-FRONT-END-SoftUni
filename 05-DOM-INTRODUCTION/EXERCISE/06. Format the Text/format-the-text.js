f803da355cb9c7c922b6927992fec3d15e49165b
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-03-22 17:49:29 +0200

    More solutions

--- 05-DOM-INTRODUCTION/EXERCISE/06. Format the Text/format-the-text.js
+++ 05-DOM-INTRODUCTION/EXERCISE/06. Format the Text/format-the-text.js
@@ -1,3 +1,30 @@
 function solve() {
-  // TODO
+    let textInput = document.getElementById('input');
+    let sentences = textInput.value
+        .split('.')
+        .map(s => s.trim())
+        .filter(s => s.length > 0);
+
+    let outputText = document.getElementById('output');
+    outputText.innerHTML = '';
+
+    let temp = [];
+
+    for (let sentence of sentences) {
+        temp.push(sentence);
+
+        if (temp.length === 3) {
+            let paragraph = document.createElement('p');
+            paragraph.textContent = temp.join('. ') + '.';
+            outputText.appendChild(paragraph);
+            temp = [];
+        }
+    }
+
+    // Ако останат 1-2 изречения накрая
+    if (temp.length > 0) {
+        let paragraph = document.createElement('p');
+        paragraph.textContent = temp.join('. ') + '.';
+        outputText.appendChild(paragraph);
+    }
 }
