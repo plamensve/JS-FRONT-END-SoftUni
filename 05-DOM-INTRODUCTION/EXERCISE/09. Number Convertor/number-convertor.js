5f245919f25cb2aa19d4d733eb7bd13305001f6f
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-03-23 17:36:36 +0200

    More solutions

--- 05-DOM-INTRODUCTION/EXERCISE/09. Number Convertor/number-convertor.js
+++ 05-DOM-INTRODUCTION/EXERCISE/09. Number Convertor/number-convertor.js
@@ -1,3 +1,18 @@
 function solve() {
-    // TODO
+    let inputNumber = document.getElementById('input');
+    let numberToConvert = Number(inputNumber.value);
+
+    let option = document.getElementById('selectMenuTo');
+    let convertor = option.value;
+
+    let result = '';
+
+    if (convertor === 'binary') {
+        result = numberToConvert.toString(2);
+    } else if (convertor === 'hexadecimal') {
+        result = numberToConvert.toString(16).toUpperCase();
+    }
+
+    let resultNumber = document.getElementById('result');
+    resultNumber.value = result;
 }
