bad085c685ae9b0df2a04546677c0c0568372948
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-03-23 16:02:21 +0200

    More solutions

--- 05-DOM-INTRODUCTION/EXERCISE/07. Hell's Kitchen/hells-kitchen.js
+++ 05-DOM-INTRODUCTION/EXERCISE/07. Hell's Kitchen/hells-kitchen.js
@@ -1,3 +1,60 @@
 function solve() {
-    //TODO
+    let info = document.querySelectorAll('textarea')
+
+    let workers = [];
+    for (let x of info){
+        let pattern = /"([^"]*)"/g;
+        let matches = x.value.match(pattern);
+        if (matches) {
+            workers.push(...matches.map(match => match.replace(/"/g, '')));
+        }
+    }
+
+    let salary = {};
+    for (let x of workers) {
+        let [restaurantName, workersStr] = x.split(' - ');
+        let workersArr = workersStr.split(', ');
+
+        if (!salary.hasOwnProperty(restaurantName)){
+            salary[restaurantName] = {};
+        }
+
+        for (let worker of workersArr) {
+            let [name, sal] = worker.split(' ');
+            salary[restaurantName][name] = Number(sal);
+        }
+    }
+
+    let bestRestaurantName = '';
+    let avgSalaryOfBestRestaurant = 0;
+    let maxSalary = 0;
+    let outputWorkers = {};
+
+    for (let [name, workers] of Object.entries(salary)) {
+        let salaries = Object.values(workers);
+        let avgSalary = salaries.reduce((a, b) => a + b, 0) / salaries.length;
+        let currMaxSalary = Math.max(...salaries);
+
+        if (avgSalary > avgSalaryOfBestRestaurant) {
+            bestRestaurantName = name;
+            avgSalaryOfBestRestaurant = avgSalary;
+            maxSalary = currMaxSalary;
+            outputWorkers = workers;
+        }
+    }
+
+    let sortedWorkers = Object.entries(outputWorkers)
+        .sort((a, b) => b[1] - a[1]);
+
+    let result = sortedWorkers
+        .map(([name, salary]) => `Name: ${name} With Salary: ${salary}`)
+        .join(' ');
+
+    // ВАЖНО: пишем в <p>, не в <span>
+    let bestRest = document.getElementById('bestRestaurant');
+    bestRest.querySelector('p').textContent =
+        `Name: ${bestRestaurantName} Average Salary: ${avgSalaryOfBestRestaurant.toFixed(2)} Best Salary: ${maxSalary.toFixed(2)}`;
+
+    let bestRestWorkers = document.getElementById('workers');
+    bestRestWorkers.querySelector('p').textContent = result;
 }
--- 05-DOM-INTRODUCTION/EXERCISE/08. Generate Report/generate-report.js
+++ 05-DOM-INTRODUCTION/EXERCISE/08. Generate Report/generate-report.js
@@ -1,3 +1,32 @@
 function solve() {
-    //TODO
+    let headers = document.querySelectorAll('thead [type="checkbox"]')
+
+    let selected = new Map();
+
+    for (let i = 0; i < headers.length; i++) {
+        if (headers[i].checked) {
+            selected.set(i, headers[i].name)
+        }
+    }
+
+    let result = [];
+    let rows = document.querySelectorAll('tbody tr')
+
+    for (let row of rows){
+        let record = {};
+
+        for (let i = 0; i < row.children.length; i++){
+            if (selected.has(i)) {
+                let propName  = selected.get(i);
+                let col = row.children[i]
+
+                record[propName] = col.textContent;
+            }
+        }
+
+        result.push(record)
+    }
+
+    let output = document.getElementById('output');
+    output.value = JSON.stringify(result);
 }
