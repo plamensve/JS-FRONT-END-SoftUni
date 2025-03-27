e10fbd1063b66bd818b15a3af5794f2a9f6fa522
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-03-27 13:10:59 +0200

    More solutions

--- 06-DOM-MANIPULATION-AND-EVENTS/LAB/03. Add-Delete/add-delete.js
+++ 06-DOM-MANIPULATION-AND-EVENTS/LAB/03. Add-Delete/add-delete.js
@@ -1,3 +1,24 @@
 function addItem() {
-    // TODO
+    // Onclick addItem - add input value
+    // add [Delete] button after the added element
+    // create delete function to delete the element
+
+    let btn = document.getElementById('newItemText');
+    let btnValue = btn.value;
+
+    let element = document.getElementById('items');
+    let newLi = document.createElement('li');
+
+    let deleteLink = document.createElement('a');
+    deleteLink.href = '#';
+    deleteLink.textContent = '[Delete]';
+
+    deleteLink.addEventListener("click", () => newLi.remove());
+
+    newLi.textContent = btnValue;
+    newLi.appendChild(deleteLink);
+    element.appendChild(newLi);
+
+    btn.value = '';
+
 }
