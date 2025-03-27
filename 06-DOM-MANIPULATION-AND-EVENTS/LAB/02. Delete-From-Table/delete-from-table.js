c62c82c60921ba6464d99a893ed914871e879563
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-03-27 11:29:44 +0200

    More solutions

--- 06-DOM-MANIPULATION-AND-EVENTS/LAB/02. Delete-From-Table/delete-from-table.js
+++ 06-DOM-MANIPULATION-AND-EVENTS/LAB/02. Delete-From-Table/delete-from-table.js
@@ -1,3 +1,24 @@
 function deleteByEmail() {
-    //TODO
+    let getInput = document.querySelector('[name="email"]');
+    let inputValue = getInput.value;
+
+    let emailList = Array.from(document.querySelectorAll('tbody tr'));
+
+    for (let row of emailList) {
+        let cells = row.querySelectorAll('td')
+
+        let email = cells[1].textContent
+
+        if (inputValue === email) {
+            row.remove()
+
+            let result = document.getElementById('result')
+            result.textContent = 'Deleted.'
+        } else {
+            let result = document.getElementById('result')
+            result.textContent = 'Not found.'
+        }
+
+    }
+
 }
