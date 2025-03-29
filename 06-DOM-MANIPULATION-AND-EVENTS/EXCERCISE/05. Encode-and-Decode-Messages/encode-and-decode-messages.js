81a24f9629621f9019e0088d19c3f251b9f77c71
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-03-29 20:14:00 +0200

    More solutions

--- 06-DOM-MANIPULATION-AND-EVENTS/EXCERCISE/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ 06-DOM-MANIPULATION-AND-EVENTS/EXCERCISE/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -1,5 +1,41 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    let encodeValue = document.getElementById('encode')
+    let textArea = encodeValue.querySelector('textarea')
+    let encodeBtn = encodeValue.querySelector('button')
+    encodeBtn.addEventListener('click', encode)
+
+    function encode(event){
+        event.preventDefault()
+
+        let msg = textArea.value
+        let decodeMsg = '';
+
+        for (let char of msg){
+            let currentChar = char.charCodeAt(0) + 1
+            decodeMsg += String.fromCharCode(currentChar)
+        }
+
+        textArea.value = '';
+
+        let decodeValue = document.getElementById('decode');
+        let textAreaDecode = decodeValue.querySelector('textarea');
+        textAreaDecode.textContent = decodeMsg;
+
+        let decodeBtn = decodeValue.querySelector('button')
+        decodeBtn.addEventListener('click', decode)
+
+        let encodedMsg = '';
+
+        function decode (event){
+            event.preventDefault()
+
+            for (let char of decodeMsg){
+                let currentChar = char.charCodeAt(0) - 1
+                encodedMsg += String.fromCharCode(currentChar)
+                textAreaDecode.textContent = encodedMsg;
+            }
+        }
+    }
 }
