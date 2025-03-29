dc43679407f9fc375e4c22afc4e8102d55a1dde0
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-03-29 18:29:22 +0200

    More solutions

--- 06-DOM-MANIPULATION-AND-EVENTS/EXCERCISE/03. Locked-Profile/locked-profile.js
+++ 06-DOM-MANIPULATION-AND-EVENTS/EXCERCISE/03. Locked-Profile/locked-profile.js
@@ -1,5 +1,32 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    let btn = document.querySelectorAll('button')
+    for (let x of btn) {
+        x.addEventListener('click', showMore)
+    }
+
+    function showMore(event) {
+        event.preventDefault()
+        let clickedBtn = event.target
+
+        let profileDiv = clickedBtn.closest('.profile');
+        let radioGroup = profileDiv.querySelector('.radio-group');
+        let inputs = radioGroup.querySelectorAll('input');
+
+        if (inputs[0].checked) {
+            return;
+        }
+
+        if (clickedBtn.textContent === 'Show more') {
+            let sibling = clickedBtn.previousElementSibling;
+            sibling.style.display = 'block';
+            clickedBtn.textContent = 'Show less'
+
+        } else if (clickedBtn.textContent === 'Show less'){
+            let sibling = clickedBtn.previousElementSibling;
+            sibling.style.display = 'none';
+            clickedBtn.textContent = 'Show more'
+        }
+    }
 }
--- 06-DOM-MANIPULATION-AND-EVENTS/EXCERCISE/04. Fill-Dropdown/fill-dropdown.js
+++ 06-DOM-MANIPULATION-AND-EVENTS/EXCERCISE/04. Fill-Dropdown/fill-dropdown.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    let itemText = document.getElementById('newItemText')
+    let itemValue = document.getElementById('newItemValue')
+
+    let btnAdd = document.querySelector('[type="submit"]')
+    btnAdd.addEventListener('click', addOption)
+
+    function addOption(event){
+        event.preventDefault()
+
+        let text = itemText.value
+        let value = itemValue.value
+
+        let newOptionElement = document.createElement('option')
+        newOptionElement.textContent = `${text}: ${value}`
+
+        let menuItem = document.getElementById('menu')
+        menuItem.appendChild(newOptionElement)
+
+        itemText.value = '';
+        itemValue.value = '';
+    }
 }
