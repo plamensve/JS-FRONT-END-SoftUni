29d645ca06ad4f063e5fbebdb6e9590a03403b84
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-04-04 18:39:01 +0300

    More solutions

--- 07-HTTP-AND-REST/02. GitHub-Repos/scripts/app.js
+++ 07-HTTP-AND-REST/02. GitHub-Repos/scripts/app.js
@@ -1,3 +1,34 @@
+let btn = document.querySelector('button')
+btn.addEventListener('click', loadRepos)
+
+
 function loadRepos() {
-   console.log("TODO...");
+    fetch('https://api.github.com/users/plamensve/repos', {
+        headers: {
+            'Authorization': 'token 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'
+        }
+    })
+        .then(onResponse)
+        .then(onData)
+        .catch(onError);
+
+
+    function onResponse(response){
+        if (!response.ok){
+            throw new Error('Something went wrong')
+        }
+
+        return response.text();
+    }
+    function onData(data){
+        let output = document.getElementById('res')
+        output.textContent = data;
+    }
+
+    function onError(error){
+        let output = document.getElementById('res')
+        output.textContent = error;
+    }
 }
+
+
--- 07-HTTP-AND-REST/03. Github-Repos-by-Username/scripts/app.js
+++ 07-HTTP-AND-REST/03. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,40 @@
 function loadRepos() {
-	console.log("TODO...");
+    let btn = document.querySelector('button');
+    btn.addEventListener('click', loadRepos)
+    let user = document.getElementById('username').value;
+
+    fetch(`https://api.github.com/users/${user}/repos`, {
+
+    })
+        .then(onResponse)
+        .then(onData)
+        .catch(onError);
+
+    function onResponse(response){
+        if (!response.ok){
+            throw new Error('Request error' + response.status);
+        }
+
+        return response.json();
+    }
+    function onData(data){
+        let output = document.getElementById('repos');
+        output.replaceChildren();
+        for (let repo of data){
+            let newLi = document.createElement('li');
+            let a = document.createElement('a');
+            a.href = repo.html_url
+            a.textContent = `${repo.name}/${user}`
+
+            newLi.appendChild(a);
+            output.appendChild(newLi);
+        }
+
+    }
+
+    function onError(error){
+        console.log(error)
+    }
 }
+
+
